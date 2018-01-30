847856be40b1918857ddd02edccabf5ebf7b1256
Filippov_AA <Filippov_AA@directum.ru>
2018-01-30 18:41:35 +0400

    Modified lib + gov report

---
+++ SomeActions/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TrelloNet;
+
+namespace SomeActions
+{
+  class Program
+  {
+
+    static string UserToken = "11";
+    static string DeveloperKey = "11";
+
+    static void Main(string[] args)
+    {
+      ITrello trello = new Trello(DeveloperKey, true);
+
+
+      //trello.
+      //httpWebRequest.Proxy.Credentials = CredentialCache.DefaultCredentials;
+      //var cc = CredentialCache.DefaultCredentials;
+
+
+      trello.Authorize(UserToken);
+      
+
+      // Get the authenticated member
+      //Member me = trello.Members.Me();
+      //Console.WriteLine(me.FullName);
+
+
+      
+
+      //var myBoard = trello.Boards.ForMember(me);
+      var board = trello.Boards.WithId("5a5c32dcac1b363264c4e25a");
+      var lists = trello.Lists.ForBoard(board); //Where(l=>l.Name == "Неделя 29.01 - 02.02").FirstOrDefault()
+
+      foreach (var list in lists)
+      {
+        var membersTotalPlan = new Dictionary<string, double>(); // MemberId
+
+        Console.WriteLine(list.Name + ":");
+
+        var cards = trello.Cards.ForList(list);
+
+        if (cards.Count() == 0)
+        {
+          Console.WriteLine("Нихт картен");
+        }
+
+        foreach (var card in cards)
+        {
+          //Console.WriteLine(card.Name);
+          int memCount = card.IdMembers.Count;
+
+          var pd = trello.Cards.CardPluginData(card.Id);
+          int cardPoints = 0;
+          if (pd.Count > 0)
+          {
+            cardPoints = pd.Where(p => p.IdPlugin == "59d4ef8cfea15a55b0086614").FirstOrDefault().ParsedValue.Points;
+          }
+
+          foreach (string memId in card.IdMembers)
+          {
+            if (membersTotalPlan.Keys.Contains(memId))
+            {
+              membersTotalPlan[memId] += (Convert.ToDouble(cardPoints) / memCount);
+            }
+            else
+            {
+              membersTotalPlan.Add(memId, Convert.ToDouble(cardPoints) / memCount);
+            }
+
+            //var cardMember = trello.Members.WithId(memId);
+            //Console.Write(cardMember.FullName + " ");
+            //var tc = trello.Cards.WithId(card.Id); // 5a6aeba0a2cd0bebfce3d617
+          }
+
+
+          //Console.WriteLine(card.);
+        }
+
+        foreach (string memId in membersTotalPlan.Keys)
+        {
+          var cardMember = trello.Members.WithId(memId);
+          Console.WriteLine(string.Format("{0}: {1}", cardMember.FullName, membersTotalPlan[memId]));
+        }
+        Console.WriteLine();
+      }
+      
+
+
+      /*
+      var lists = trello.Lists.ForBoard(myBoard).Where(l=>l.Name != "TO DO");
+
+      foreach (var list in lists)
+      {
+        Console.WriteLine(list.Name);
+        var cards = trello.Cards.ForList(list);
+        foreach (var card in cards)
+        {
+          Console.WriteLine(card.Name);
+          //Console.Write(string.Join(", ", card.IdMembers));
+          //Console.WriteLine(": ");
+          foreach (var memId in card.IdMembers)
+          {
+            var cardMember = trello.Members.WithId(memId);
+            Console.Write(cardMember.FullName + " ");
+          }
+
+          //Console.WriteLine(card.);
+        }
+
+
+        Console.WriteLine();
+      }
+       */
+
+      //Console.WriteLine(me.FullName);
+
+      // Get a board
+      //Board theTrelloDevBoard = trello.Boards.WithId("4d5ea62fd76aa1136000000c");
+      //Console.WriteLine(theTrelloDevBoard.Name);
+
+      Console.ReadKey();
+      /*var myBoard = trello.Boards.Add("My Board");
+
+      var todoList = trello.Lists.Add("To Do", myBoard);
+      trello.Lists.Add("Doing", myBoard);
+      trello.Lists.Add("Done", myBoard);
+
+      trello.Cards.Add("My card", todoList);
+
+      foreach (var list in trello.Lists.ForBoard(myBoard))
+          Console.WriteLine(list.Name);*/
+    }
+  }
+}
---
+++ TrelloNet/Cards/PluginData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+using RestSharp;
+using RestSharp.Deserializers;
+using TrelloNet.Internal;
+
+namespace TrelloNet
+{
+  public class PluginData
+  {
+    public string Id { get; set; }
+    public string IdPlugin { get; set; }
+    public string Scope { get; set; }
+    public string IdModel { get; set; }
+    public string Value { get; set; }
+    public PluginDataValue ParsedValue { get; set; }
+    public string Access { get; set; }
+
+    public class PluginDataValue
+    {
+      public int Points { get; set; }
+      public List<string> PointsHistory { get; set; }
+    }
+
+    [OnDeserialized]
+    private void AfterDeserialization(StreamingContext context)
+    {
+      if (IdPlugin == "59d4ef8cfea15a55b0086614")
+      {
+        ParsedValue = JsonConvert.DeserializeObject<PluginDataValue>(Value);
+      }
+    }
+  }
+}
